d1368f4df29c72ceaf5dad57b44b4341020349fe
Asya Starickaya <astaritsky@icloud.com>
2023-03-13 22:32:20 +0300

    add GetOrderTests

---
+++ src/test/java/GetOrderTest.java
@@ -0,0 +1,29 @@
+import Pojo.GetOrdersFullJson;
+import Pojo.Orders;
+import io.restassured.RestAssured;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class GetOrderTest {
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "http://qa-scooter.praktikum-services.ru/";
+    }
+
+    @Test
+    public void getOrder() {
+        GetOrdersFullJson getOrdersJson = given()
+                .header("Content-type", "application/json")
+                .log().all()
+                .get("/api/v1/orders")
+                .as(GetOrdersFullJson.class);
+
+        Assert.assertNotNull(getOrdersJson.getOrders());
+    }
+}
